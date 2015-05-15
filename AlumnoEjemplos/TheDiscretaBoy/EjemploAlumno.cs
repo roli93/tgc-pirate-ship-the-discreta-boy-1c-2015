5f1a0e9fdf6688b1049b9042ba6491d3f68381b4
JessSaavedra <jessicasaavedraval@gmail.com>
2015-05-15 00:54:57 -0300

    agregamos cielo

--- AlumnoEjemplos/TheDiscretaBoy/EjemploAlumno.cs
+++ AlumnoEjemplos/TheDiscretaBoy/EjemploAlumno.cs
@@ -12,6 +12,7 @@ using TgcViewer.Utils.TgcSceneLoader;
 using TgcViewer.Utils.Input;
 using Microsoft.DirectX.DirectInput;
 using TgcViewer.Utils.TgcSkeletalAnimation;
+using TgcViewer.Utils.Terrain;
 
 namespace AlumnoEjemplos.MiGrupo
 {
@@ -24,6 +25,7 @@ namespace AlumnoEjemplos.MiGrupo
         TgcMesh canon;
         TgcMesh mesh;
         TgcBox water;
+        TgcSkyBox cielo;
         Vector3 lastYposition = new Vector3(0, 0, 0);
         GenericShip ship;
 
@@ -50,7 +52,7 @@ namespace AlumnoEjemplos.MiGrupo
 
 
             Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
-
+            string texturesPath = GuiController.Instance.ExamplesMediaDir + "Texturas\\Quake\\SkyBox LostAtSeaDay\\";
             TgcSceneLoader loader = new TgcSceneLoader();
             TgcScene sceneShip = loader.loadSceneFromFile(GuiController.Instance.ExamplesMediaDir + "MeshCreator\\Meshes\\Vehiculos\\Canoa\\Canoa-TgcScene.xml");
             mesh = sceneShip.Meshes[0];
@@ -58,14 +60,28 @@ namespace AlumnoEjemplos.MiGrupo
             canon = sceneCanon.Meshes[0];
 
             water = TgcBox.fromSize(new Vector3(0, 0, 0), new Vector3(5000, 1, 5000), Color.Aqua);
-            water.setTexture(TgcTexture.createTexture(d3dDevice, GuiController.Instance.ExamplesDir + "Optimizacion\\Isla\\Textures\\agua 10.jpg"));
-            ship = new GenericShip(mesh, new Vector3(0, 10, 0));
+            water.setTexture(TgcTexture.createTexture(d3dDevice, texturesPath + "lostatseaday_dn.jpg"));
+            ship = new GenericShip(mesh, new Vector3(0, 2, 0));
 
+          
+
+            cielo = new TgcSkyBox();
+            cielo.Center = new Vector3(0, 500, 0);
+            cielo.Size = new Vector3(5000, 1000, 5000);
+            cielo.setFaceTexture(TgcSkyBox.SkyFaces.Up, texturesPath + "lostatseaday_up.jpg");
+            cielo.setFaceTexture(TgcSkyBox.SkyFaces.Down, texturesPath + "lostatseaday_dn.jpg");
+            cielo.setFaceTexture(TgcSkyBox.SkyFaces.Left, texturesPath + "lostatseaday_lf.jpg");
+            cielo.setFaceTexture(TgcSkyBox.SkyFaces.Right, texturesPath + "lostatseaday_rt.jpg");
+            cielo.setFaceTexture(TgcSkyBox.SkyFaces.Front, texturesPath + "lostatseaday_ft.jpg");
+            cielo.setFaceTexture(TgcSkyBox.SkyFaces.Back, texturesPath + "lostatseaday_bk.jpg");
 
 
             GuiController.Instance.ThirdPersonCamera.Enable = true;
             Vector3 CameraPosition = ship.Position();
-            GuiController.Instance.ThirdPersonCamera.setCamera(CameraPosition, 250, 500);
+            GuiController.Instance.ThirdPersonCamera.setCamera(CameraPosition, 100, -750);
+            //GuiController.Instance.RotCamera.Enable = true;
+   
+            cielo.updateValues();
 
 
         }
@@ -92,7 +108,7 @@ namespace AlumnoEjemplos.MiGrupo
             if (d3dInput.keyDown(Key.S))
             {
                 ship.desacelerate(speed);
-                canon.moveOrientedY(-speed);
+                canon.Position = ship.Position() + new Vector3(0, 1, 0);
 
             }
 
@@ -128,15 +144,11 @@ namespace AlumnoEjemplos.MiGrupo
 
 
 
-
-
-
-
-
             GuiController.Instance.ThirdPersonCamera.Target = ship.Position();
             ship.render();
             water.render();
             canon.render();
+            cielo.render();
 
 
 
@@ -148,6 +160,7 @@ namespace AlumnoEjemplos.MiGrupo
             water.dispose();
             ship.dispose();
             canon.dispose();
+            cielo.dispose();
 
         }
 
